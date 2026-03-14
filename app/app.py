3a88dbf145b467c9c2c81b8ed00d7460d7066edf
Karthikeyan <karthikjr0111@gmail.com>
2026-03-14 13:07:21 +0530

    Initial DevOps CI/CD project

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {"id": 1, "task": "Learn DevOps"},
+    {"id": 2, "task": "Build CI/CD Pipeline"}
+]
+
+@app.route("/")
+def home():
+    return "DevOps Python App Running"
+
+@app.route("/tasks")
+def get_tasks():
+    return jsonify(tasks)
+
+if __name__ == "__main__":
+    app.run(debug=True)
